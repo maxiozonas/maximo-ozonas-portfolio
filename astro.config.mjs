9975f502e199f2bf1c2c50dc44da19d42a80f61b
Maximo Ozonas <maxiozonas10@gmail.com>
2025-05-26 10:02:28 -0300

    Fix para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    includeFiles: [".env", ".env.production"] // Incluir archivos de entorno
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
